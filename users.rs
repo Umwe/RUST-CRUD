0cdd46e036f93b3a41367e4f2ce5601fa0dae57a
Umwe <99398212+Umwe@users.noreply.github.com>
2024-09-04 22:42:18 +0200

    Create users.rs

---
+++ users.rs
@@ -0,0 +1,16 @@
+All Users:
+User { id: 1, name: "Alice", email: "alice@example.com" }
+User { id: 2, name: "Bob", email: "bob@example.com" }
+
+User with ID 1: User { id: 1, name: "Alice", email: "alice@example.com" }
+
+User with ID 2 updated.
+
+All Users after update:
+User { id: 1, name: "Alice", email: "alice@example.com" }
+User { id: 2, name: "Bob Updated", email: "bob_updated@example.com" }
+
+User with ID 1 deleted.
+
+All Users after delete:
+User { id: 2, name: "Bob Updated", email: "bob_updated@example.com" }
